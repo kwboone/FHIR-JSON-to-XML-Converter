5f2ad84e414237a9abf2e8cb1c3c649151f4427f
Mike Carifio <mike@carif.io>
2016-02-25 13:12:03 -0500

    Test parsing a MedicationDispense.

---
+++ FHIR JSON to XML Converter/test/medication.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+/**
+ * Mike Carifio <mike@carif.io> (https://mike.carif.io/)
+ */
+
+var fhir2xml = require('../dist/node-fhir-convert'); // hacked version of fhir-convert.js that exports certain functions
+
+var fs = require('fs');
+var json = JSON.parse(fs.readFileSync('medication.json', "utf8"));
+
+fhir2xml.FHIRConverter(2);
+var xml = fhir2xml.toXML(json);
+console.log(xml);
